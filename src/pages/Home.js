149e0939b6bd5fd69d2a570c1b1f5294c1cdff41
unknown <mail.mukeshyadav@gmail.com>
2022-09-26 14:39:51 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 import familyPic from './pics/family.jpg';
+import yadav_family from './pics/yadav_family.jpg';
 
 
 export default function Home() {
   return <div style={{ textAlign: 'center' }}>
     {/* */}
     <h1>Welcome to Yadav's Family HomePage.</h1>
-    <img 
-      src={familyPic} 
-      alt="Yadav Family" 
-      style={{ borderColor: 'black' }} 
-    />
+      <div className="item">
+          <img 
+            src={familyPic} 
+            alt="Yadav Family" 
+            style={{ borderColor: 'black' }} />
+      </div>      
+      <div className="item">
+        <img
+          src={yadav_family}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
   </div>
 }
